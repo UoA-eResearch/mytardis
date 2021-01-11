95ed3cbd09c049ff403b0fb54271c941697e770b
Noel <noel.zeng@auckland.ac.nz>
2021-01-12 11:34:04 +1300

    Fix lint problems in daterangefilter

--- assets/js/apps/search/components/filters/date-filter/DateRangeFilter.jsx
+++ assets/js/apps/search/components/filters/date-filter/DateRangeFilter.jsx
@@ -1,18 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import PropTypes from 'prop-types';
-import Datetime from 'react-datetime';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import PropTypes from "prop-types";
+import Datetime from "react-datetime";
+import moment from "moment";
 
 // React Datetime requires CSS to work.
-import 'react-datetime/css/react-datetime.css';
+import "react-datetime/css/react-datetime.css";
+
+const DATE_FORMAT = "YYYY-MM-DD";
 
 const isNone = (value) => {
     // We need empty string to represent an empty field for text fields
     return value === undefined || value === null || value === "";
-}
+};
 
+/**
+ * Checks if a filter value doesn't have actual value in it.
+ * @param {*} value The local value to check.
+ */
 const isValueEmpty = (value) => {
     if (isNone(value)) {
         return true;
@@ -22,6 +28,10 @@ const isValueEmpty = (value) => {
     return isNone(start) && isNone(end);
 };
 
+/**
+ * Converts the value we are using in this filter to the format expected by the search API.
+ * @param {*} localValue The local value to convert
+ */
 const toSubmitValue = localValue => {
     // Replace empty string value with null to represent null parameter value.
     if (!localValue) {
@@ -29,28 +39,28 @@ const toSubmitValue = localValue => {
     }
     const submitValue = [];
     if (!isNone(localValue.start)) {
-        if (typeof localValue.start == "object") {
-            submitValue.push({
-                op: ">=",
-                content: localValue.start.toISOString()
-            });
-        }
+        submitValue.push({
+            op: ">=",
+            content: localValue.start.format(DATE_FORMAT)
+        });
     }
     if (!isNone(localValue.end)) {
-        if (typeof localValue.end == "object") {
-            submitValue.push({
-                op: "<=",
-                content:localValue.end.toISOString()
-            })
-        }
+        submitValue.push({
+            op: "<=",
+            content: localValue.end.toISOString(DATE_FORMAT)
+        });
     }
-    if (submitValue.length == 0) {
+    if (submitValue.length === 0) {
         // Return a null to represent no filter value.
         return null;
     }
     return submitValue;
-}
+};
 
+/**
+ * Converts value in search API format to the value used in this filter.
+ * @param {*} submitValue The value in the search API format
+ */
 const toLocalValue = submitValue => {
     if (!submitValue) {
         return {start: null, end: null};
@@ -68,20 +78,29 @@ const toLocalValue = submitValue => {
         localValue.end = moment(endValue[0].content);
     }
     return localValue;
+};
+
+/**
+ * Merges options for this filter with defaults, if no value is specified
+ * for any of the options. 
+ * @param {*} options Original parsed options
+ */
+function mergeOptionsWithDefaults(options) {
+    const newOptions = Object.assign({}, options);
+    if (!newOptions.hintStart) {
+        const weekAgoDate = moment().subtract(1, "week").format(DATE_FORMAT);
+        newOptions.hintStart = `YYYY-MM-DD (e.g. ${weekAgoDate})`;
+    }
+    if (!newOptions.hintEnd) {
+        const todayDate = moment().format(DATE_FORMAT);
+        newOptions.hintEnd = `YYYY-MM-DD (e.g. ${todayDate})`;
+    }
+    return newOptions;
 }
 
-const DateRangeFilter = ({ id, value, options, onValueChange }) => {
+const DateRangeFilter = ({ id = "missingFilterName", value, options, onValueChange }) => {
     // Make a copy of the options first.
-    options = Object.assign({},options);
-    if (!id) {
-        id = "missingFilterName";
-    }
-    if (!options.hintStart) {
-        options.hintStart = "(e.g. 2020-12-30)";
-    }
-    if (!options.hintEnd) {
-        options.hintEnd = "(e.g. 2021-01-15)";
-    }
+    options = mergeOptionsWithDefaults(options);
 
     const [localValue, setLocalValue] = useState(toLocalValue(value));
 
@@ -90,25 +109,26 @@ const DateRangeFilter = ({ id, value, options, onValueChange }) => {
         // for when the filter value is externally updated
         // e.g. from URL.
         setLocalValue(toLocalValue(value));
-    },[value]);
+    }, [value]);
 
-    const handleValueChange = (type, valueFromForm) => {
-        // Copy the value object, then assign new value into either "start" or "end".
+    const handleStartValueChange = valueFromForm => {
+        // Copy the value object, then assign new value into the start field.
         const newValue = Object.assign({}, localValue);
-        newValue[type] = valueFromForm;
-        // React Datetime returns a string if the user enters invalid information.
-        if (type === "start" && typeof newValue.start == "object") {
-            if (!options.hideEnd && (!newValue.end || newValue.start.isAfter(newValue.end))) {
-                // If we are setting start date and there is no end date OR end date is earlier
-                //than start date, we auto-fill end date to be same as start date
-                newValue.end = newValue.start;
-            }
-        } else if (type === "end" && typeof newValue.end == "object") {
-            if (!options.hideStart && (!newValue.start || newValue.end.isBefore(newValue.start))) {
-                // If setting end date and there's no start date OR if new end date is before the start date,
-                // we auto-fill start date to be same as end date.
-                newValue.start = newValue.end;
-            }
+        newValue.start = valueFromForm;
+        if (!options.hideEnd && (!newValue.end || newValue.start.isAfter(newValue.end))) {
+            newValue.end = newValue.start;
+        }
+        setLocalValue(newValue);
+    };
+
+    const handleEndValueChange = valueFromForm => {
+        // Copy the value object, then assign new value into the start field.
+        const newValue = Object.assign({}, localValue);
+        newValue.end = valueFromForm;
+        if (!options.hideStart && (!newValue.start || newValue.end.isBefore(newValue.start))) {
+            // If setting end date and there's no start date OR if new end date is before the start date,
+            // we auto-fill start date to be same as end date.
+            newValue.start = newValue.end;
         }
         setLocalValue(newValue);
     };
@@ -117,27 +137,18 @@ const DateRangeFilter = ({ id, value, options, onValueChange }) => {
     // But we should be able to clear a field if there's a value on the filter.
     const canChangeValue = !isValueEmpty(localValue) || !isNone(value);
 
-    const isValidEndDate = (current) => {
-        if (!localValue.start) {
-            // If there is no start date, user can set any end date
-            return true;
-        }
-        // If there is a start date, then we check the date is after start date.
-        return current.isSameOrAfter(localValue.start);
-    }
-
     const handleSubmit = (e) => {
         e.preventDefault();
         if (!canChangeValue) {
             return;
         }
-        const value = toSubmitValue(localValue);
-        onValueChange(value);
+        const newValue = toSubmitValue(localValue);
+        onValueChange(newValue);
     };
 
     // Give the input boxes ids so labels can be tied back to the field.
-    const startFieldId = "start-"+id,
-        endFieldId = "end-"+id;
+    const startFieldId = "start-" + id,
+        endFieldId = "end-" + id;
 
     return (
         <Form className="date-range-filter" onSubmit={handleSubmit}>
@@ -146,10 +157,10 @@ const DateRangeFilter = ({ id, value, options, onValueChange }) => {
                     <Form.Label htmlFor={startFieldId} srOnly={options.hideLabels}>Start</Form.Label>
                     <Datetime
                         value={localValue.start}
-                        onChange={handleValueChange.bind(this, "start")}
+                        onChange={handleStartValueChange}
                         inputProps={{ placeholder: options.hintStart, id: startFieldId }}
                         closeOnSelect={true}
-                        dateFormat="YYYY-MM-DD"
+                        dateFormat={DATE_FORMAT}
                         timeFormat={false}
                         // Hack for react-datetime bug:
                         // https://github.com/arqex/react-datetime/issues/760
@@ -162,10 +173,10 @@ const DateRangeFilter = ({ id, value, options, onValueChange }) => {
                     <Form.Label htmlFor={endFieldId} srOnly={options.hideLabels}>End</Form.Label>
                     <Datetime
                         value={localValue.end}
-                        onChange={handleValueChange.bind(this, "end")}
+                        onChange={handleEndValueChange}
                         inputProps={{ placeholder: options.hintEnd, id: endFieldId }}
                         closeOnSelect={true}
-                        dateFormat="YYYY-MM-DD"
+                        dateFormat={DATE_FORMAT}
                         timeFormat={false}
                         // Hack for react-datetime bug: 
                         // https://github.com/arqex/react-datetime/issues/760
@@ -183,7 +194,7 @@ const DateRangeFilter = ({ id, value, options, onValueChange }) => {
             </Button>
         </Form>
     );
-}
+};
 
 DateRangeFilter.propTypes = {
     id: PropTypes.string.isRequired,
@@ -192,9 +203,10 @@ DateRangeFilter.propTypes = {
         hideStart: PropTypes.bool,
         hideEnd: PropTypes.bool,
         hintStart: PropTypes.string,
-        hintEnd: PropTypes.string
+        hintEnd: PropTypes.string,
+        hideLabels: PropTypes.bool
     }),
     onValueChange: PropTypes.func.isRequired
-}
+};
 
 export default DateRangeFilter;
