61183979b03cec6029c032e27a0103ef37f1cf8e
Mike Laverick <mike.laverick@auckland.ac.nz>
2021-03-02 14:09:52 +1300

    Update openid_migration migration files to use latest tardis migrations

--- tardis/apps/openid_migration/migrations/0001_initial.py
+++ tardis/apps/openid_migration/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('tardis_portal', '0012_userauthentication_approved'),
+        ('tardis_portal', '0045_auto_20210128_1536'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
