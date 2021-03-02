b2b06ee110364c4b628e1eee362da739d2773e1a
Mike Laverick <mike.laverick@auckland.ac.nz>
2021-03-02 13:56:00 +1300

    Update openid_migration to use ExperimentACL, not ObjectACL

--- tardis/apps/openid_migration/migrations/0001_initial.py
+++ tardis/apps/openid_migration/migrations/0001_initial.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
             name='OpenidACLMigration',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('acl_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='tardis_portal.ObjectACL')),
+                ('acl_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='tardis_portal.ExperimentACL')),
             ],
         ),
         migrations.CreateModel(
