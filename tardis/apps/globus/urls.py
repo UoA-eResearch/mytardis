56d9dff15d6fcdda762c332c7cb76b8700466a3e
Mike Laverick <mike.laverick@auckland.ac.nz>
2021-05-05 10:03:50 +1200

    Tweak globus url

--- tardis/apps/globus/urls.py
+++ tardis/apps/globus/urls.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from django.conf.urls import url
 
 from . import views
 
+app_name = "tardis.apps.globus"
 urlpatterns = [
     url(r'^accessible-hosts/$', views.get_accessible_hosts),
     url(r'^$', views.globus_initiate, name='index'),
