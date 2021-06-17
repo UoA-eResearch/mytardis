000bdf3b0839675f7cd5097f034624c50f87fced
Mike Laverick <mike.laverick@auckland.ac.nz>
2021-06-17 17:15:18 +1200

    Draft APIs for Globus transfers

--- tardis/apps/globus/api.py
+++ tardis/apps/globus/api.py
@@ -4,3 +4,141 @@ Implemented with Tastypie.
 
 .. moduleauthor:: Mike Laverick <mikelaverick@btinternet.com>
 '''
+
+import json
+import logging
+#from datetime import datetime
+
+from django.conf import settings
+#from django.template.defaultfilters import filesizeformat
+
+from tastypie import fields
+from tastypie.resources import Resource, Bundle
+from tastypie.serializers import Serializer
+#from tastypie.exceptions import ImmediateHttpResponse
+#from tastypie.http import HttpUnauthorized
+
+from tardis.tardis_portal.api import default_authentication
+#from tardis.tardis_portal.auth import decorators as authz
+from .models import RemoteHost, TransferLog
+
+
+class PrettyJSONSerializer(Serializer):
+    json_indent = 2
+
+    def to_json(self, data, options=None):
+        logging.info("The data is " + str(data))
+        options = options or {}
+        data = self.to_simple(data, options)
+        return json.dumps(data, cls=json.JSONEncoder,
+                          sort_keys=True, ensure_ascii=False,
+                          indent=self.json_indent) + "\n"
+
+
+if settings.DEBUG:
+    default_serializer = PrettyJSONSerializer()
+else:
+    default_serializer = Serializer()
+
+# API object to return accessible hosts
+class RemoteHostsObject(object):
+    def __init__(self, hosts=None):
+        self.hosts = hosts
+
+# API object to check selected objects and return invalid options
+class DownloadCartObject(object):
+    def __init__(self, projects=None, experiments=None, Datasets=None,
+                 Datafiles=None):
+        self.projects = projects
+        self.experiments = experiments
+        self.datasets = datasets
+        self.datafiles = datafiles
+
+# API object to submit objects for globus transfer
+# Could just re-use the cart object? and for the size API?
+
+#class GlobusSubmissionObject(object):
+#    def __init__(self, objects=None, id=None):
+#        self.objects = objects
+#        self.id = id
+
+
+class RemoteHostsAppResource(Resource):
+    """Tastypie resource for RemoteHosts"""
+    hosts = fields.ApiField(attribute='hosts', null=True)
+
+    class Meta:
+        resource_name = 'get-remotehosts'
+        list_allowed_methods = ['get']
+        serializer = default_serializer
+        authentication = default_authentication
+        object_class = RemoteHostsObject
+        always_return_data = True
+
+    def get_object_list(self, request):
+        if not request.user.is_authenticated:
+            raise Exception(message="User must be logged in to retrieve remote hosts")
+        hosts = RemoteHost.objects.filter(users=request.user)
+        for group in request.user.groups.all():
+            hosts |= RemoteHost.objects.filter(groups=group)
+        response = []
+        for host in hosts.distinct():
+            response.append(host)
+        return [RemoteHostsObject(id=1, hosts=response)]
+
+    def obj_get_list(self, bundle, **kwargs):
+        return self.get_object_list(bundle.request)
+
+
+class ValidateCartAppResource(Resource):
+    """Tastypie resource to validate Download Cart contents"""
+    projects = fields.ApiField(attribute='projects', null=True)
+    experiments = fields.ApiField(attribute='experiments', null=True)
+    datasets = fields.ApiField(attribute='datasets', null=True)
+    datafiles = fields.ApiField(attribute='datafiles', null=True)
+
+    class Meta:
+        resource_name = 'validate-cart'
+        list_allowed_methods = ['post']
+        serializer = default_serializer
+        authentication = default_authentication
+        object_class = DownloadCartObject
+        always_return_data = True
+
+
+    def get_object_list(self, request):
+        return request
+
+    def obj_get_list(self, bundle, **kwargs):
+        return self.get_object_list(bundle.request)
+
+    def obj_create(self, bundle, **kwargs):
+        bundle = self.create_return_invalid(bundle)
+        return bundle
+
+
+    def create_return_invalid(self, bundle):
+        user = bundle.request.user
+        if not user.is_authenticated:
+            raise Exception(message="User must be logged in to validate download cart")
+        groups = user.groups.all()
+
+        host_id = bundle.data.get('remote_host', None)
+        proj_ids = bundle.data.get('projects', [])
+        exp_ids = bundle.data.get('experiments', [])
+        set_ids = bundle.data.get('datasets', [])
+        file_ids = bundle.data.get('datafiles', [])
+        if not host_id:
+            raise Exception(message="Please specify a remote host")
+        if not any([proj_ids, exp_id, set_ids, file_ids]):
+            raise Exception(message="Please provide Projects/Experiments/Datasets/Datafiles for validation")
+        # Query for related projects, and unpack into list here for efficiency
+        related_projects = [*{*RemoteHost.objects.filter(id=host_id).prefetch_related('projects'
+                                ).values_list("projects__id", flat=True)}]
+
+        proj_list, exp_list, set_list, file_list = [], [], [], []
+        proj_list = [id for id in proj_ids if id not in related_projects]
+
+        bundle.obj = DownloadCartObject(projects=proj_list, experiments=exp_list,
+                                        datasets=set_list, datafiles=file_list)
+        return bundle
