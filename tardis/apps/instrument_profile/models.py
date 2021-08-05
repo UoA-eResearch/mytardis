cc876db168cfb2d7371a90161a4d66bb768593d4
Chris Seal <c.seal@auckland.ac.nz>
2021-08-05 17:49:07 +1200

    Fix indents

--- tardis/apps/instrument_profile/models.py
+++ tardis/apps/instrument_profile/models.py
@@ -1,6 +1,6 @@
- import logging
+import logging
 
- from django.db import models
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -8,8 +8,9 @@ from tardis.tardis_portal.models.instrument import Instrument
 
 logger = logging.getLogger(__name__)
 
+
 class InstrumentProfile(models.Model):
-    """ A model to implement the minimum metadata schema for an instrument
+    """A model to implement the minimum metadata schema for an instrument
 
     :attribute instrument: A OneToOneField pointing to a related Instrument
     :attribute landing_page: A URLField that holds the instrument landing page (defaults to MyTardis)
@@ -23,11 +24,20 @@ class InstrumentProfile(models.Model):
     :attribute pid: A CharField holding the chosen PID
     """
 
-    instrument = models.OneToOne(Instrument, on_delete=models.CASCADE, related_name='profile')
+    instrument = models.OneToOne(
+        Instrument, on_delete=models.CASCADE, related_name="profile"
+    )
     landing_page = models.URLField(null=True, blank=True)
-    owner = models.CharField(max_length=400, null=False, blank=False, default='No Owner - Please Udate')
+    owner = models.CharField(
+        max_length=400, null=False, blank=False, default="No Owner - Please Udate"
+    )
     owner_contact = models.CharField(max_length=400, null=True, blank=True)
-    manufacturer = models.CharField(max_length=400, null=False, blank=False, default='No Manufacturer - Please Update')
+    manufacturer = models.CharField(
+        max_length=400,
+        null=False,
+        blank=False,
+        default="No Manufacturer - Please Update",
+    )
     instrument_model = models.CharField(max_length=400, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
     instrument_type = models.CharField(max_length=400, null=True, blank=True)
@@ -35,10 +45,10 @@ class InstrumentProfile(models.Model):
     pid = models.CharField(max_length=400, null=True, blank=True, unique=True)
 
     def __str__(self):
-        return instrument.name
+        return self.instrument.name
+
 
 @receiver(post_save, sender=Instrument, dispatch_uid="create_instrument_profile")
 def create_instrument_profile(sender, instance, created, **kwargs):
     if created:
         InstrumentProfile(instrument=instance).save()
-    
