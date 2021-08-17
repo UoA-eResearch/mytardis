6b48bbef031da7d5422bedd3a2929edfbfa15821
Chris Seal <c.seal@auckland.ac.nz>
2021-08-17 12:11:37 +1200

    Fix broken import for reverse

--- tardis/apps/instrument_profile/models.py
+++ tardis/apps/instrument_profile/models.py
@@ -3,6 +3,7 @@ import logging
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 
 from tardis.tardis_portal.models.instrument import Instrument
 
