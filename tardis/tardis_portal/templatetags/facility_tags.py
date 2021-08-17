2ad71e5374aa2af9e9d513690e93337e991dc899
Chris Seal <c.seal@auckland.ac.nz>
2021-08-18 10:03:11 +1200

    Toggle button on or off if the instrument profile is active or not

--- tardis/tardis_portal/templatetags/facility_tags.py
+++ tardis/tardis_portal/templatetags/facility_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 from ..models.facility import is_facility_manager
 
 register = template.Library()
@@ -10,6 +11,15 @@ def check_if_facility_manager(request):
     Custom template filter to identify whether a user is a
     facility manager.
     """
-    if (request.user.is_authenticated):
+    if request.user.is_authenticated:
         return is_facility_manager(request.user)
     return False
+
+
+@register.filter
+def check_if_instrument_profile_active(request):
+    """
+    Custom template filter to check if the instrument_profile app
+    is active.
+    """
+    return "tardis.apps.instrument_profile" in settings.INSTALLED_APPS
