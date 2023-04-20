7c2a01c9cd9df633bdd0aa225f35edd945e0e957
Chris Seal <c.seal@auckland.ac.nz>
2023-04-20 16:15:00 +1200

    Initial commit. Added models and basic functionality

---
+++ tardis/apps/autoarchive/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from tardis.apps.autoarchive.models import DataFileAutoArchive
+
+admin.site.register(DataFileAutoArchive)
---
+++ tardis/apps/autoarchive/apps.py
@@ -0,0 +1,6 @@
+from tardis.app_config import AbstractTardisAppConfig
+
+
+class DataClassificationConfig(AbstractTardisAppConfig):
+    name = "tardis.apps.autoarchive"
+    verbose_name = "autoarchive"
