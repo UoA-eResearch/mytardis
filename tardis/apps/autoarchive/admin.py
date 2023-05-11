71334aa6c91e75e362d6759b125a428c838db96d
Chris Seal <c.seal@auckland.ac.nz>
2023-05-11 14:09:55 +1200

    fix: add experiment autoarchive to admin

--- tardis/apps/autoarchive/admin.py
+++ tardis/apps/autoarchive/admin.py
@@ -1,6 +1,13 @@
+from django.conf import settings
 from django.contrib import admin
 
-from tardis.apps.autoarchive.models import DataFileAutoArchive, ProjectAutoArchive
+from tardis.apps.autoarchive.models import (
+    DataFileAutoArchive,
+    ExperimentAutoArchive,
+    ProjectAutoArchive,
+)
 
 admin.site.register(DataFileAutoArchive)
-admin.site.register(ProjectAutoArchive)
+if "tardis.apps.projects" in settings.INSTALLED_APPS:
+    admin.site.register(ProjectAutoArchive)
+admin.site.register(ExperimentAutoArchive)
