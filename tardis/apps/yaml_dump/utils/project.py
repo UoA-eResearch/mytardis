ecd7f874923fc185ee355dc899771c6a8b3b457a
Chris Seal <c.seal@auckland.ac.nz>
2024-04-18 15:45:23 +1200

    Updated api for testing on dev

--- tardis/apps/yaml_dump/utils/project.py
+++ tardis/apps/yaml_dump/utils/project.py
@@ -3,53 +3,48 @@
 A set of functions to wrangle the data into an appropriate format for export via YAML
 """
 
-import contextlib
 from typing import Any, Dict
 
 from django.conf import settings
 
-from tardis.apps.yaml_dump.models.project import Project
-from tardis.apps.yaml_dump.models.username import Username
-from tardis.apps.yaml_dump.utils.utility import (
-    add_acls_to_dataclass,
-    add_data_classification_to_dataclass,
-    add_metadata_to_dataclass,
-)
-
 INGESTED = 5
 
 
-# TODO: https://aucklanduni.atlassian.net/browse/IDS-686
-def wrangle_project_into_IDW_YAML(project: Dict[str, Any]) -> Project:
-    """Flatten the structure of a project into the format suited to dump in a YAML
+def wrangle_project_into_IDW_YAML(project: Dict[str, Any]) -> Dict[str, Any]:
+    """Flatten the structure data into the format suited to dump in a YAML
 
     Args:
         project (Dict[str,Any]): A project dictionary produced by the serializer
 
     Returns:
-        Project: The YAML object representing the project
+        Dict[str,Any]: The flattened dictionary
     """
-    project_dc = Project(
-        data_status=INGESTED,  # INGESTED flag is set since we can get the data from MyTardis
-        description=project["description"],
-        name=project["name"],
-        principal_investigator=Username(
-            f'{project["principal_investigator"]["username"]}',
-        ),
-    )
-    with contextlib.suppress(IndexError, KeyError):
-        project_dc.object_schema = (
-            project["projectparameterset_set"][0]["schema"]["namespace"] or ""
-        )
-    # TODO: https://aucklanduni.atlassian.net/browse/IDS-685
-    project_dc = add_metadata_to_dataclass(project_dc, project)
-    project_dc = add_acls_to_dataclass(project_dc, project)
-    project_dc = add_data_classification_to_dataclass(project_dc, project)
-
+    return_dict = {
+        "data_status": INGESTED,  # INGESTED flag is set since we can get the data from MyTardis
+        "description": project["description"],
+        "name": project["name"],
+        "object_schema": project["parameterset"][0]["schema"]["namespace"],
+        "principal_investigator": project["principal_investigator"]["username"],
+    }
+    if "tardis.apps.dataclassification" in settings.INSTALLED_APPS:
+        return_dict["data_classification"] = project["dataclassification"]
     if (
         "tardis.apps.identifiers" in settings.INSTALLED_APPS
         and "project" in settings.OBJECTS_WITH_IDENTIFIERS
     ):
-        identifiers = [value["identifier"] for value in project["identifiers"]]
-        project_dc.identifiers = identifiers
-    return project_dc
+        return_dict["identifiers"] = project["identifiers"]
+
+
+"""
+    !Project
+data_classification: null
+data_status: null
+description: ''
+groups: []
+identifiers:
+- helen-project
+metadata: null
+name: Helen's project
+object_schema: ''
+principal_investigator: !Username 'szen012'
+users: []"""
