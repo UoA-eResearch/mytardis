66907e1bf147d069e0ed4a19b973fdddd6dbe4b9
Noel <noel.zeng@auckland.ac.nz>
2024-07-08 21:53:18 +1200

    Fix pylint errors

--- tardis/apps/idw_download/views/idw.py
+++ tardis/apps/idw_download/views/idw.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-# @login_required
+@login_required
 class IDWIndexView(TemplateView):
     template_name = "idw/index.html"
