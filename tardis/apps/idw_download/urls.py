e87f5c6cffa246bfdde04758cc963b982823f91c
Noel <noel.zeng@auckland.ac.nz>
2024-07-09 09:27:37 +1200

    Add login_required to idw views

--- tardis/apps/idw_download/urls.py
+++ tardis/apps/idw_download/urls.py
@@ -1,10 +1,13 @@
 from django.urls import re_path
+from django.contrib.auth.decorators import login_required
 
 from tardis.apps.idw_download.views.idwyaml import IDWYAMLView
 from tardis.apps.idw_download.views.idw import IDWIndexView
 
+
 app_name = "tardis.apps.idw_download"
 
-urlpatterns = [re_path("idw-yaml/", IDWYAMLView.as_view(), name="idw-yaml"),
-    re_path("^$", IDWIndexView.as_view(), name="index")
+urlpatterns = [
+    re_path("idw-yaml/", login_required(IDWYAMLView.as_view()), name="idw-yaml"),
+    re_path("^$", login_required(IDWIndexView.as_view()), name="index")
 ]
